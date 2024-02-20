29c738ac1cb5e666de393b770fcef6fdd3daa8dd
WinnerMatawin <160159032+WinnerMatawin@users.noreply.github.com>
2024-02-21 03:34:08 +0700

    Add files via upload

---
+++ www.py
@@ -0,0 +1,42 @@
+import math
+c = input().strip()
+if c == "S" :
+    m = int(input())
+    q = 1
+    r = 0
+    t = 1
+    k = 1
+    n = 3
+    x = 3
+    i = 0
+    p = ""
+    while i < m :
+        if 4*q + r - t < n*t :
+            p = p + str(n)
+            i = i+1
+            a = 10*(r-n*t)
+            n = 10*(3*q+r)//t-(10*n)
+            q = q*10
+            r = a
+        else:
+            a = (2*q + r)*x
+            b = ((7*q*k) + 2 + (x*r))//(x*t)
+            q = k*q
+            t = t*x
+            x = x+2
+            k = k+1
+            n = b
+            r = a
+    p = p[0] + '.' + p[1:]
+    print('pi =', p)
+
+elif c == "R" :
+    n = int(input())
+    p = math.sqrt(12)*sum((((-3)**(-1*k))/(2*k +1)) for k in range(n+1))
+    p = round(p, 12)
+    print('pi =', p)
+elif c == "P" :
+    p = math.sqrt(7+math.sqrt(6+math.sqrt(5)))
+    p = round(p,6)
+    print('pi =', p)
+else: print("Invalid")
