5c3987cdb95c25a3853b15dc3b70b6907676839c
WinnerMatawin <160159032+WinnerMatawin@users.noreply.github.com>
2024-02-20 19:49:05 +0700

    Add files via upload

---
+++ 04_Loop/www.py
@@ -0,0 +1,12 @@
+import math
+a = float(input())
+l = 0
+u = a
+x = (l+u)/2
+while abs(a-10**x) > (10**-10)*max(a, 10**x) :
+    if 10**x > a :
+        u = x
+    if 10**x < a :
+        l = x
+    x = (l+u)/2
+print(round(x,6))
